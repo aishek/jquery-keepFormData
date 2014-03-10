fbcc317a20ba488bd188300b9fbe3208493cfa30
Alexandr Borisov <aishek@gmail.com>
2014-03-10 11:14:15 +0400

    renamed src/_namespace.js -> src/namespace.min.js for codeclimate exclude

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         src: [
-          'src/_namespace.js',
+          'src/namespace.min.js',
           'src/util.js',
           'src/input.js',
           'src/select.js',
